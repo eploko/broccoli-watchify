cbf0cf377f7fb12679f206ea773d16b3a9ad3b77
Stefan Penner <stefan.penner@gmail.com>
2016-08-24 22:48:14 -0700

    update + improve stuff
    * use broccol-plugin baseclass
    * persistentOutput when possible
    * get output stable if possible

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ var RSVP = require('rsvp');
 var mkdirp = require('mkdirp');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var Writer = require('broccoli-writer');
+var Plugin = require('broccoli-plugin');
+var md5Hex = require('md5hex');
 
 module.exports = Watchify;
 function Watchify(inputTree, options) {
   if (!(this instanceof Watchify)) {
     return new Watchify(inputTree, options);
   }
+  Plugin.call(this, [inputTree], options);
+  this._persistentOutput = true;
   this.options = _.extend(this.getDefaultOptions(), options);
-  this.inputTree = inputTree;
   this.watchifyData = watchify.args;
+
+  this._fileToChecksumMap = Object.create(null); // TODO: extract SP
 }
 
-Watchify.prototype = Object.create(Writer.prototype);
+Watchify.prototype = Object.create(Plugin.prototype);
 Watchify.prototype.constructor = Watchify;
 Watchify.prototype.getDefaultOptions = function () {
   return {
@@ -28,42 +32,55 @@ Watchify.prototype.getDefaultOptions = function () {
   };
 };
 
-Watchify.prototype.write = function (readTree, destDir) {
+Watchify.prototype.writeFileIfContentChanged = function(fullPath, content) {
+  var previous = this._fileToChecksumMap[fullPath];
+  var next = md5Hex(content);
+
+  if (previous === next) {
+    // hit
+  } else {
+    fs.writeFileSync(fullPath, content);
+    this._fileToChecksumMap[fullPath] = next; // update map
+  }
+};
+
+Watchify.prototype.build = function () {
   var plugin = this;
-  var options = this.options;
 
-  return readTree(this.inputTree).then(function (srcDir) {
-    var outputFile = destDir + '/' + options.outputFile;
-    mkdirp.sync(path.basename(outputFile));
+  var srcDir = this.inputPaths[0];
+  var destDir = this.outputPath;
 
-    options.browserify.basedir = srcDir;
+  var outputFile = destDir + '/' + this.options.outputFile;
 
-    var browserifyOptions;
+  mkdirp.sync(path.basename(outputFile));
 
-    if (options.cache) {
-      browserifyOptions = _.extend(options.browserify, plugin.watchifyData);
-    } else {
-      browserifyOptions = options.browserify;
-    }
+  this.options.browserify.basedir = srcDir;
 
-    var w = browserify(browserifyOptions);
-    if (options.cache) { w = watchify(w); }
+  var browserifyOptions;
 
-    console.log(browserifyOptions)
-    options.init(w);
-    return new RSVP.Promise(function (resolve, reject) {
-      w.bundle(function (err, data) {
-        if (err) {
-          reject(err);
-        } else {
-          try {
-            fs.writeFileSync(outputFile, data);
-            resolve(destDir);
-          } catch (e) {
-            reject(e);
-          }
+  if (this.options.cache) {
+    browserifyOptions = _.extend(this.options.browserify, this.watchifyData);
+  } else {
+    browserifyOptions = this.options.browserify;
+  }
+
+  var w = browserify(browserifyOptions);
+  if (this.options.cache) { w = watchify(w); }
+
+  this.options.init(w);
+
+  return new RSVP.Promise(function (resolve, reject) {
+    w.bundle(function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          plugin.writeFileIfContentChanged(outputFile, data);
+          resolve(destDir);
+        } catch (e) {
+          reject(e);
         }
-      });
+      }
     });
   });
 };

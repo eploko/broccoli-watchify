8ed7f1f6a9c13b84ce55116491cdd21fcfb57ca4
Stefan Penner <stefan.penner@gmail.com>
2016-08-24 22:38:04 -0700

    asdf

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
-var es6 = require('es6-promise');
+var RSVP = require('rsvp');
 var mkdirp = require('mkdirp');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var Writer = require('broccoli-writer');
 
+module.exports = Watchify;
 function Watchify(inputTree, options) {
   if (!(this instanceof Watchify)) {
     return new Watchify(inputTree, options);
@@ -28,31 +29,41 @@ Watchify.prototype.getDefaultOptions = function () {
 };
 
 Watchify.prototype.write = function (readTree, destDir) {
-  var self = this;
-  var o = this.options;
+  var plugin = this;
+  var options = this.options;
 
   return readTree(this.inputTree).then(function (srcDir) {
-    mkdirp.sync(path.join(destDir, path.dirname(o.outputFile)));
+    var outputFile = destDir + '/' + options.outputFile;
+    mkdirp.sync(path.basename(outputFile));
 
-    o.browserify.basedir = srcDir;
+    options.browserify.basedir = srcDir;
 
-    var browserifyOptions = o.cache ? _.extend(o.browserify, self.watchifyData) : o.browserify;
+    var browserifyOptions;
+
+    if (options.cache) {
+      browserifyOptions = _.extend(options.browserify, plugin.watchifyData);
+    } else {
+      browserifyOptions = options.browserify;
+    }
+
     var w = browserify(browserifyOptions);
-    if (o.cache) { w = watchify(w); }
-      
-    o.init(w);
+    if (options.cache) { w = watchify(w); }
 
-    return new es6.Promise(function (resolve, reject) {
+    console.log(browserifyOptions)
+    options.init(w);
+    return new RSVP.Promise(function (resolve, reject) {
       w.bundle(function (err, data) {
         if (err) {
           reject(err);
         } else {
-          fs.writeFileSync(path.join(destDir, o.outputFile), data);
-          resolve(destDir);
+          try {
+            fs.writeFileSync(outputFile, data);
+            resolve(destDir);
+          } catch (e) {
+            reject(e);
+          }
         }
       });
     });
   });
 };
-
-module.exports = Watchify;
